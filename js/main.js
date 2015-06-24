6633a1b204bd27f4b8b8fbc318e61e72ce223518
paigesaez <paigesaez@gmail.com>
2015-06-24 13:42:13 -0500

    massive refactoring across the board, including changes to the css, js and html. I restyled the entire page, added new ajax calls

--- js/main.js
+++ js/main.js
@@ -1,48 +1,23 @@
-  
+
   var BASE_URL = 'https://api.spotify.com/v1/';
-  var SEARCH_LIMIT = 5;
-  var RELATED_LIMIT = 6;
-  var $ajaxlog = $('#ajaxlog');  
+  var SEARCH_LIMIT = 1;
+  var RELATED_LIMIT = 3;
   var $searchResults = $('#searchresults');
   var $selectedArtistTemplate = $('#selectedartisttemplate');
   var $relatedArtistTemplate = $('#relatedartisttemplate');
-  var $spotifyResults = $('#spotifyresults');  
+  var $spotifyResults = $('#spotifyresults');
   var searchResultData = {};
+  var relatedArtists = [];
 
-  
-  
-  
-  
-  $(document).ajaxComplete(function(event, request, settings) {
-    $ajaxlog.append('<li>Request Complete.</li>');
-  });
-  $(document).ajaxError(function(event, request, settings, thrownError) {
-    $ajaxlog.append('<li>Error requesting page <b>' + settings.url + '</b></li>');
-    $ajaxlog.append('<li>Error Thrown: <b>' + thrownError + '</b></li>');
-  });
-  $(document).ajaxSend(function(event, request, settings) {
-    $ajaxlog.append('<li>Starting request at ' + settings.url + '</li>');
-  });
-  $(document).ajaxStart(function() {
-    $ajaxlog.append('<li>ajax call started</li>');
-  });
-  $(document).ajaxStop(function() {
-    $ajaxlog.append('<li>ajax call stopped</li>');
-  });
-  $(document).ajaxSuccess(function(event, request, settings) {
-    $ajaxlog.append('<li>Successful Request!</li>');
-  });  
-  
-  $('.modal-trigger').leanModal();
-  
   $('#btnsearchartists').on('click', function(e) {
+    // console.log( $('#txtArtistSearch') );
     var query = $('#txtArtistSearch').val();
     if (query.length > 2) {
-      $searchResults.html('');      
+      $searchResults.html('');
       searchArtists(query);
     }
   });
-  
+
 
   $('body').on('click', '.artist', function(e) {
     e.preventDefault();
@@ -54,68 +29,143 @@
     console.log('renderedTemplate: ', $renderedTemplate);
     $spotifyResults.html($renderedTemplate);
     getRelatedByID(selectedID);
-    //$searchResults.html('');
-    //$('#card-toggler').slideToggle(3000);
     $('#searchcard').addClass('hidden');
     $('#hiddenrow').removeClass('hidden').addClass('animated bounceInDown');
   });
-  
-  
-  $('body').on('click', '#searchagainbutton', function(e) {
-    $('#searchcard').removeClass('hidden').addClass('animated bounceInDown');
-    $('#hiddenrow').removeClass('animated bounceInDown').addClass('hidden'); 
-  });
-  
-  
-searchArtists('Dave Matthews');
-  
-  function getRelatedByID(artistID) {
-    return $.get(BASE_URL+'artists/'+artistID+'/related-artists')
-      .pipe(trimResults)
-      .pipe(renderRelatedTemplate);
-  }
 
-  function trimResults(response) {
-    if (response.artists.length > RELATED_LIMIT) {
-      response.artists = response.artists.slice(0, RELATED_LIMIT);
-    }
-    return response;
-  }
 
-  function renderRelatedTemplate(relatedArtists) {
-    console.log('relatedArtists: ', relatedArtists);
-    console.log('passed to template2: ', relatedArtists.artists);
-    var $renderedTemplate = $relatedArtistTemplate.tmpl(relatedArtists.artists);
-    console.log('renderedTemplate: ', $renderedTemplate);
-    $('#relatedartists').html($renderedTemplate);
-  } 
-  
+// ajax call 1: search for the artist, return the artist object & their ID
   function searchArtists(query) {
     var oData = {
       q: query,
       type: 'artist',
       limit: SEARCH_LIMIT,
       offset: 0,
-      market: 'US'
+      market: 'US',
+      id: ' '
     };
+
     var url = BASE_URL+'search';
-    return $.get(url, oData)
-      .pipe(renderSearchResults);
+    // return $.get(url, oData);
+    $.get(url, oData)
+    //pass the artist ID to getRelatedByID
+      .pipe(function(data) {
+        console.log("data...");
+        console.log(data.artists.items[0].id);
+        getRelatedByID(data.artists.items[0].id);
+      });
   }
+// ajax call 2: using the artist ID, get 3 related artists
+  function getRelatedByID(id) {
+    $.get(BASE_URL+'artists/'+id+'/related-artists')
+      .pipe(function(data){
+          storeArtists(trimResults(data.artists));
+      });
+  }
+
+function trimResults(artists) {
+  if (artists.length > RELATED_LIMIT) {
+   artists = artists.slice(0, RELATED_LIMIT);
+  }
+  return artists;
+}
+
+function storeArtists(artists) {
+  for (var i = artists.length - 1; i >= 0; i--) {
+
+    var artist = {
+      id: artists[i].id,
+      name: artists[i].name,
+      image: artists[i].images[0],
+      link: artists[i].uri,
+      albums: []
+    };
+
+    relatedArtists.push(artist);
+  }
+
+  for (var x = 0; x < 3; x ++){
+    getArtistAlbums(relatedArtists[x]);
+  }
+}
+
+function getArtistAlbums(artist) {
+  var oData = {
+    offset: 0,
+    limit: 3,
+    album_type: 'album'
+  };
+
+  var url = BASE_URL+'artists/'+artist.id+'/albums';
+   $.get(url, oData)
+    .pipe(function(data){
+      artist.albums = addAlbumData(data);
+    });
+}
+
+function addAlbumData(albums){
+  var albumData = [];
+
+  for (var n = albums.items.length - 1; n >= 0; n--) {
+
+    var album = {
+      id: albums.items[n].id,
+      name: albums.items[n].name,
+      image: albums.items[n].images[0].url,
+      link: albums.items[n].uri
+    };
+    albumData.push(album);
+  }
+  return albumData;
+}
+
+
+// // render the artists object to html:
 
   function renderSearchResults(response) {
-    searchResultData = response;
-    var artists = response.artists.items;
-    var result = '';
-    for (var i = 0; i < artists.length; i++) {
-      var artistName = artists[i].name;
-      var artistID = artists[i].id;
-      result += '<li><a class="artist" data-selected-index="'+i+'" data-artist-name="'+artistName+'" href="'+artistID+'">'+artistName+'</a></li>';
-    }
-    $searchResults.html(result);  
+//look to his example on github
+
+
+ var ul = $('<ul>');
+
+ for (var i = artists.length - 1; i >= 0; i--) {
+
+  var li = $('<li>').addClass('artist').append(artists[i]);
+   ul.append(li);
   }
 
+ $('#searchResults').append(ul);
 
-function justDisplayResponse(response) {
-  console.log('response: ', response);
-}
+
+
+   }
+
+
+
+//        result += '<li>Artist Image</li><li><a class="artist" data-selected-index="'+i+'" data-artist-name="'+artistName+'" href="'+artistID+'">'+artistName+'</a></li>';
+
+//       //print artists and their albums
+
+//         result +='<li><a class="albumName" data-selected-index="'+i+'" data-album-name="'+albumName+'" href="'+spotifyLink+'">'+albumName+'</a></li><li><a class="albumArt" data-selected-index="'+i+'" data-album-art="'+albumArt+'" href="'+spotifyLink+'">'+albumArt+'</a></li><li><a class="playableLink" data-selected-index="'+i+'" data-playable-link="'+playableLink+'" href="'+playableLink+'">'+playableLink+'</a></li>';
+//       }
+
+
+//     }
+//     $searchResults.html(result);
+// }
+
+
+//   function renderRelatedTemplate(relatedArtists) {
+//     console.log('relatedArtists: ', relatedArtists);
+//     console.log('passed to template2: ', relatedArtists.artists);
+//     var $renderedTemplate = $relatedArtistTemplate.tmpl(relatedArtists.artists);
+//     console.log('renderedTemplate: ', $renderedTemplate);
+//     $('#relatedartists').html($renderedTemplate);
+//   }
+
+
+
+
+// function justDisplayResponse(response) {
+//   console.log('response: ', response);
+// }
